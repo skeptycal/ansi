cf3a1ce73cc51d6d4ae6cd03518884cd2b52e633
Michael Treanor <26148512+skeptycal@users.noreply.github.com>
2021-04-24 13:11:28 -0500

    (GoBot) devtag version v0.4.0: update examples and deps

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,52 +3,17 @@ package main
 import (
 	"fmt"
 
-	"github.com/skeptycal/cli"
+	"github.com/skeptycal/ansi"
 )
 
+var green = ansi.NewColor(ansi.Green, ansi.Black, ansi.Bold)
+
+func Green(args ...interface{}) {
+	fmt.Print(green)
+	fmt.Print(args...)
+	fmt.Println(ansi.Reset)
+}
+
 func main() {
-	// main := ansi.NewColor(7, 32, 40)
-
-	var t = cli.NewStdout(nil)
-
-	fmt.Println(t)
-
-	// t.CLS()
-	t.DevMode(true)
-	fmt.Println(t)
-
-	// t.UseColor(true)
-
-	// t.Hr()
-
-	// t.Println("This is bright white text.")
-	// t.Println("This is bright white text.")
-
-	// t.SetColor(ansi.NewColor(ansi.Green, 0, 0))
-	// fmt.Println(t)
-
-	// t.Println("Set UseColor(false).")
-	// t.UseColor(false)
-	// t.Println("This is green text. (but isn't showing green - UseColor == false!)")
-	// t.Br()
-
-	// t.Println("Set UseColor(true).")
-	// t.UseColor(true)
-	// t.Println("This is green text. (color is back on now - UseColor == true!)")
-
-	// t.Println("Any text that is printed after the color is set will print in the default color.")
-	// t.Println("Set t.SetColor(ansi.NewColor(ansi.Blue, 0, 0)).")
-	// t.SetColor(ansi.NewColor(ansi.Blue, 0, 0))
-	// t.Println("Any text that is printed after the color is set will print in the default color.")
-	// t.SetColor(ansi.NewColor(ansi.Yellow, 0, ansi.Italics))
-	// t.Println("Any text that is printed after the color is set will print in the default color.")
-	// t.SetColor(ansi.NewColor(ansi.Black, ansi.RedBackground, ansi.Underline))
-	// t.Println("Any text that is printed after the color is set will print in the default color.")
-
-	// // fmt.Println(t.Writer)
-
-	// t.Println("This is green text.")
-	// t.Println("This is green text.")
-	// t.Println("This is green text.")
-	// t.Println("This is green text.")
+	Green("Green Title")
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,5 @@ go 1.16
 
 require (
 	github.com/sirupsen/logrus v1.8.1
-	github.com/skeptycal/cli v0.1.0
 	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57 // indirect
 )
