8137451a4020c246f85531487bc5f578e5f91493
Michael Treanor <26148512+skeptycal@users.noreply.github.com>
2021-04-25 23:23:24 -0500

    (GoBot) devtag version v0.4.0-1619410879442894: add var "current" for current color

--- ansi.go
+++ ansi.go
@@ -14,16 +14,57 @@
 // Many of the ANSI cursor codes are from https://github.com/k0kubun/go-ansi
 package ansi
 
-const (
-	// ResetEffects string = "\033[39;49;0m"  // Reset foreground, background with no effects
-	// DefaultText  string = "\033[39;49;22m" // Default text color and intensity
-	Default    string = "\033[39;49m" // Default foreground and background color
-	Reset      string = "\033[0m"     // Turn off all effects
-	SetInverse string = "\x1b[7m"
-	FmtANSI    string = "\033[%dm"
+import (
+	"fmt"
+	"strings"
 )
 
-var ResetBytes []byte = []byte(Reset)
+const (
+	// set ANSI 8-bit (256 color) foreground and background color with leading effect
+	ansiEncode = "\x1b[%d;38;5;%d;48;5;%dm"
+	fgEncode   = "\x1b[%d;38;5;%d;48;5;%dm%s%s"
+)
+
+const (
+	// ResetEffects string = "\x1b[39;49;0m"  // Reset foreground, background with no effects
+	// DefaultText  string = "\x1b[39;49;22m" // Default text color and intensity
+	Default                 string = "\x1b[39;49m" // Default foreground and background color
+	Reset                   string = "\x1b[0m"     // Turn off all effects
+	SetInverse              string = "\x1b[7m"
+	DefaultEffectString     string = "\x1b[1m"
+	DefaultForegroundString string = "\x1b[37m"
+	DefaultBackgroundString string = "\x1b[47m"
+	FmtANSI                 string = "\x1b[%dm"
+)
+
+var (
+	bDefault                 []byte = []byte(Default)
+	bReset                   []byte = []byte(Reset)
+	bSetInverse              []byte = []byte(SetInverse)
+	bDefaultEffectString     []byte = []byte(DefaultEffectString)
+	bDefaultForegroundString []byte = []byte(DefaultForegroundString)
+	bDefaultBackgroundString []byte = []byte(DefaultBackgroundString)
+	bFmtANSI                 []byte = []byte(FmtANSI)
+	ResetBytes               []byte = []byte(Reset)
+
+	CurrentColor     Ansi   = NewColor(214, 0, 1)
+	DefaultColor     string = NewColor(White, Black, Normal).String()
+	bDefaultColor    []byte = []byte(DefaultColor)
+	DefaultReversed  string = NewColor(White, Black, Inverse).String()
+	bDefaultReversed []byte = []byte(DefaultColor)
+)
+
+// TODO - for dev tests ...
+var (
+	BDefault                 = bDefault
+	BReset                   = bReset
+	BSetInverse              = bSetInverse
+	BDefaultEffectString     = bDefaultEffectString
+	BDefaultForegroundString = bDefaultForegroundString
+	BDefaultBackgroundString = bDefaultBackgroundString
+	BFmtANSI                 = bFmtANSI
+	BesetBytes               = ResetBytes
+)
 
 // var Output = newAnsiStdout()
 
@@ -46,8 +87,8 @@ const (
 	Faint       // faint, decreased intensity or second color
 	Italics
 	Underline
-	// Blink
-	// FastBlink
+	// Blink  		// not implemented
+	// FastBlink 	// not implemented
 	Inverse byte = iota + 2
 	Conceal
 	Strikeout
@@ -55,7 +96,7 @@ const (
 	Subscript   byte = 74
 )
 
-// ANSI escape codes for basic foreground colors
+// ANSI escape codes for basic ANSI colors
 const (
 	Black byte = iota
 	Red
@@ -67,9 +108,49 @@ const (
 	White
 )
 
-var (
-	// Normal is the default color used when no other is specified
-	DefaultColor Ansi = NewColor(White, Black, Normal)
-	// Reversed is the default reverse color used when no other is specified
-	DefaultReversed Ansi = NewColor(White, Black, Inverse)
-)
+func Rainbow(start, end byte, s string) string {
+	return rainbow1(start, end, s)
+}
+
+func rainbow1(start, end byte, s string) string {
+	sb := strings.Builder{}
+	defer sb.Reset()
+
+	colorRange := end - start
+	strLength := len(s)
+
+	var slope float64 = float64(colorRange) / float64(strLength)
+
+	color := func(i int) byte {
+		return byte(slope*float64(i) + float64(start))
+	}
+
+	for i := 0; i < strLength; i++ {
+		c := NewColor(color(i), 0, 0)
+		sb.WriteString(fmt.Sprintf("%s%s", c, string(s[i])))
+	}
+	sb.WriteString(Reset)
+	return sb.String()
+}
+
+// constant color set
+func rainbow2(start, end byte, s string) string {
+	sb := strings.Builder{}
+	defer sb.Reset()
+
+	colorRange := end - start
+	strLength := len(s)
+
+	var slope float64 = float64(colorRange) / float64(strLength)
+
+	color := func(i int) byte {
+		return byte(slope*float64(i) + float64(start))
+	}
+
+	for i := 0; i < strLength; i++ {
+		c := NewColor(color(i), 0, 0)
+		sb.WriteString(fmt.Sprintf("%s%s", c, string(s[i])))
+	}
+	sb.WriteString(Reset)
+	return sb.String()
+}
